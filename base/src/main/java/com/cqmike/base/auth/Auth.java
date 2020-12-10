aa26b1fd7db312b5a823c45dd1fb6a4becfc11ba
cqmike <1535985458@qq.com>
2020-12-10 23:31:55 +0800

    fix: update auth
    Signed-off-by: cqmike <1535985458@qq.com>

--- base/src/main/java/com/cqmike/base/auth/Auth.java
+++ base/src/main/java/com/cqmike/base/auth/Auth.java
@@ -1,8 +1,10 @@
 package com.cqmike.base.auth;
 
+import cn.hutool.core.collection.CollUtil;
 import com.cqmike.base.util.JsonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,23 +21,36 @@ public final class Auth {
 
     public static <T> T get(String key, Class<T> tClass) {
         Map<String, Object> objectMap = map.get();
+        if (CollUtil.isEmpty(objectMap)) {
+            return null;
+        }
         Object v = objectMap.get(key);
         return JsonUtils.parse(JsonUtils.toJson(v), tClass);
     }
 
     public static <T> T get(String key, TypeReference<T> type) {
         Map<String, Object> objectMap = map.get();
+        if (CollUtil.isEmpty(objectMap)) {
+            return null;
+        }
         Object v = objectMap.get(key);
         return JsonUtils.parse(JsonUtils.toJson(v), type);
     }
 
     public static void put(String key, Object v) {
         Map<String, Object> objectMap = map.get();
+        if (CollUtil.isEmpty(objectMap)) {
+            objectMap = new HashMap<>();
+        }
         objectMap.put(key, v);
+        map.set(objectMap);
     }
 
     public static Object remove(String key) {
         Map<String, Object> objectMap = map.get();
+        if (CollUtil.isEmpty(objectMap)) {
+            return null;
+        }
         return objectMap.remove(key);
     }
 
